98e0c0ae58e02b92617058977ec891f240fa9a14
aaronBIOO <aaronnabioo@gmail.com>
2025-04-05 23:54:30 +0000

    update

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const products = [{
+    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    ratings: {
+        stars: 4.5,
+        count: 87
+    },
+    priceCents: 1090
+}, {
+    image: 'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
+    ratings: {
+        stars: 4.0,
+        count: 127
+    },
+    priceCent: 2095
+}];
