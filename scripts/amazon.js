999a42afe8e8591dd99fb664b111f46f489ed527
aaronBIOO <aaronnabioo@gmail.com>
2025-04-06 00:56:21 +0000

    More products added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCent: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4.0,
-        count: 127
-    },
-    priceCent: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCent: 799
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -42,12 +16,12 @@ products.forEach((product) => {
                 <img class="product-rating-stars"
                 src="images/ratings/rating-${product.rating.stars * 10}.png">
                 <div class="product-rating-count link-primary">
-                ${product.rating.count}
+                (${product.rating.count})
                 </div>
             </div>
 
             <div class="product-price">
-                ${(product.priceCent / 100).toFixed(2)}
+                ${(product.priceCents / 100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
