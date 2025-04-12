78ba9e43a5ffe70f7d0a2f3500d366e6766d2844
aaronBIOO <aaronnabioo@gmail.com>
2025-04-12 15:31:33 +0000

    generate HTML for order summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
+let cartSummaryHTML = '';
+
 cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
@@ -12,26 +14,26 @@ cart.forEach((cartItem) => {
         }
     });
 
-    `
-        <div class="cart-item-container">
-            <div class="delivery-date">
-                Delivery date: Tuesday, June 21
-            </div>
+    cartSummaryHTML += `
+    <div class="cart-item-container">
+        <div class="delivery-date">
+            Delivery date: Tuesday, June 21
+        </div>
 
-            <div class="cart-item-details-grid">
-                <img class="product-image"
-                src="images/products/athletic-cotton-socks-6-pairs.jpg">
+        <div class="cart-item-details-grid">
+            <img class="product-image"
+            src="${matchingProduct.image}">
 
-                <div class="cart-item-details">
+            <div class="cart-item-details">
                 <div class="product-name">
-                    Black and Gray Athletic Cotton Socks - 6 Pairs
+                    ${matchingProduct.name}
                 </div>
-                <div class="product-price">
-                    $10.90
-                </div>
+            <div class="product-price">
+                ${matchingProduct.priceCents / 100}
+            </div>
                 <div class="product-quantity">
                     <span>
-                    Quantity: <span class="quantity-label">2</span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                     </span>
                     <span class="update-quantity-link link-primary">
                     Update
@@ -40,38 +42,38 @@ cart.forEach((cartItem) => {
                     Delete
                     </span>
                 </div>
-                </div>
+            </div>
 
-                <div class="delivery-options">
-                <div class="delivery-options-title">
-                    Choose a delivery option:
-                </div>
-                <div class="delivery-option">
-                    <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                    <div>
+            <div class="delivery-options">
+            <div class="delivery-options-title">
+                Choose a delivery option:
+            </div>
+            <div class="delivery-option">
+                <input type="radio" checked
+                class="delivery-option-input"
+                name="delivery-option-1">
+                <div>
                     <div class="delivery-option-date">
                         Tuesday, June 21
                     </div>
                     <div class="delivery-option-price">
                         FREE Shipping
                     </div>
-                    </div>
                 </div>
-                <div class="delivery-option">
-                    <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                    <div>
-                    <div class="delivery-option-date">
-                        Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                        $4.99 - Shipping
-                    </div>
-                    </div>
+            </div>
+            <div class="delivery-option">
+                <input type="radio"
+                class="delivery-option-input"
+                name="delivery-option-1">
+                <div>
+                <div class="delivery-option-date">
+                    Wednesday, June 15
                 </div>
+                <div class="delivery-option-price">
+                    $4.99 - Shipping
+                </div>
+                </div>
+            </div>
                 <div class="delivery-option">
                     <input type="radio"
                     class="delivery-option-input"
@@ -85,8 +87,11 @@ cart.forEach((cartItem) => {
                     </div>
                     </div>
                 </div>
-                </div>
             </div>
         </div>
-    `
+    </div>
+    `;
 });
+
+document.querySelector('.js-order-summary')
+    .innerHTML = cartSummaryHTML;
